84036fef3e379d0d304f91afd539aeebe8b55bb3
Anjali <anjali.e.fernandes@gmail.com>
2017-06-04 21:54:51 -0700

    copy/cut to history, paste from history, remove and edit history enabled

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ Ctrl+x cuts and saves item to clipboard.
 
 Ctrl+v pastes most recent item.
 
-Ctrl+Shift+v opens the clipboard history. Use the arrow keys to scroll and press Enter to paste a selected item. Selecting the "Clear History" option will erase all items from history.
+Ctrl+Shift+v opens the clipboard history. Use the arrow keys to scroll and press Enter to paste a selected item. 
 
+Ctrl+Alt+v d opens the clipboard delete settings. Use the arrow keys to scroll and press Enter to remove a selected item. The "Clear History" option will erase all items from history.
+
+Ctrl+Alt+v e opens the clipboard editor settings. Use the arrow keys to scroll and press Enter to edit a selected item. 
 
 \!\[feature X\]\(images/feature-x.png\)
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,55 @@
 'use strict';
-// Import the module 'vscode' containing the VS Code extensibility API
-import * as vscode from 'vscode';
+import {ExtensionContext, TextEditor, TextDocument, Range, Position, QuickPickOptions, QuickPickItem, window, commands} from 'vscode';
 
-// this method is called when your extension is activated - the first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-    console.log('Congratulations, your extension "copy/paste-history" is now active!');
-
-    var clipboardArray = new Array;    
+export function activate(context: ExtensionContext) {
+    var clipboardArray = [];    
     var disposableArray = [];
-    var toBeRemoved = false;
 
     // Save all values that are copied to clipboard in array
-    function addClipboardItem(editor:vscode.TextEditor) {
-        let doc:vscode.TextDocument = editor.document;
-        // TODO add full line cut and copy
-        let text = doc.getText(new vscode.Range(editor.selection.start,editor.selection.end));
-        clipboardArray.push(text);
+    function addClipboardItem(editor: TextEditor) {
+        let doc: TextDocument = editor.document;
+        let sels = editor.selections;
+        for (var i = 0; i < sels.length; i++) {
+            let line = sels[i].active.line;
+            let text = doc.getText(new Range(sels[i].start, sels[i].end));
+            if (sels[i].isEmpty) { // Get full line if no selection highlighted
+                let lineStart = new Position(line, 0);
+                let lineEnd = new Position(line, doc.lineAt(line).range.end.character)
+                text = doc.getText(new Range(lineStart, lineEnd));
+            }
+            clipboardArray.push(text);
+        }
     }    
 
-    function makeQuickPick(clipboardArray, item?:vscode.QuickPickItem) {
+    function makeQuickPick(clipboardArray, toBeRemoved?: boolean) {
         // Create quick pick clipboard items
-        var options:vscode.QuickPickOptions = {placeHolder: "Clipboard", matchOnDescription: true, matchOnDetail: true};
-        var copiedItems:vscode.QuickPickItem[] = [{label:"-",description:"Clear History"}];
-        
-        if (toBeRemoved) {
-           clipboardArray = removeQuickPick(clipboardArray, item);
-        }
+        var options: QuickPickOptions = {placeHolder: "Clipboard", matchOnDescription: true, matchOnDetail: true};
+        var copiedItems: QuickPickItem[] = [];
+        // Add clear all history option if making removal quick pick
+        if (toBeRemoved && clipboardArray.length > 0) { copiedItems.push({ label: "0", description: "Clear All History" }); }
         // List clipboard items in order of recency
         for (var i = 0; i < clipboardArray.length; i++) {
             copiedItems.unshift({label:(i+1).toString(), description:clipboardArray[i]});
         }
-        
         return copiedItems;
     }
 
-    function removeQuickPick(clipboardArray, item: vscode.QuickPickItem) {
-        console.log(item);
-        console.log(item.description);
-        console.log(item.label);
+    function removeQuickPickItem(clipboardArray, item: QuickPickItem) {
         let index = parseInt(item.label) - 1;
-        console.log(index)
-        if (index > -1) {
-            clipboardArray.splice(index, 1);
-        }
-        toBeRemoved = false;
+        if (index > -1) { clipboardArray.splice(index, 1); }
         return clipboardArray;
     }
 
+    function editQuickPickItem(clipboardArray, item: QuickPickItem, text: string) {
+        let index = parseInt(item.label) - 1;
+        if (index > -1) {clipboardArray[index] = text; }
+        return clipboardArray;
+    }
     // Called by pasteFromClipboard command
     // Paste item if selected from options list
-    function pasteSelected(item: vscode.QuickPickItem) {
+    function pasteSelected(item: QuickPickItem) {
         let activeEditor
-        if (activeEditor = vscode.window.activeTextEditor) {    // Don't run if no active text editor instance available
+        if (activeEditor = window.activeTextEditor) {    // Don't run if no active text editor instance available
             let text = item.description;
             activeEditor.edit(function (textInserter) {
                 textInserter.delete(activeEditor.selection);    // Delete anything currently selected
@@ -61,48 +58,68 @@ export function activate(context: vscode.ExtensionContext) {
                     textInserter.insert(activeEditor.selection.start, text)     // Insert text from list
                 })
             })  
-        }
-                 
+        }         
     }
 
-    disposableArray.push(vscode.commands.registerCommand('extension.copy', () => {
-        addClipboardItem(vscode.window.activeTextEditor);
-        vscode.commands.executeCommand("editor.action.clipboardCopyAction");
+    disposableArray.push(commands.registerCommand('extension.copy', () => {
+        addClipboardItem(window.activeTextEditor);
+        commands.executeCommand("editor.action.clipboardCopyAction");
     }));
 
-    disposableArray.push(vscode.commands.registerCommand('extension.cut', () => {
-        addClipboardItem(vscode.window.activeTextEditor);
-        vscode.commands.executeCommand("editor.action.clipboardCutAction");
+    disposableArray.push(commands.registerCommand('extension.cut', () => {
+        addClipboardItem(window.activeTextEditor);
+        commands.executeCommand("editor.action.clipboardCutAction");
     }));
 
-    disposableArray.push(vscode.commands.registerCommand('extension.paste', () => {
-        vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+    disposableArray.push(commands.registerCommand('extension.paste', () => {
+        commands.executeCommand("editor.action.clipboardPasteAction");
     }));
 
-    disposableArray.push(vscode.commands.registerCommand('extension.pasteFromClipboard', () => {
+    disposableArray.push(commands.registerCommand('extension.pasteFromClipboard', () => {
         if (clipboardArray.length == 0) { 
-            vscode.window.showInformationMessage("No items in clipboard");
+            window.setStatusBarMessage("No items in clipboard");
+            window.showQuickPick(makeQuickPick(clipboardArray));
             return; 
         } else {
-            vscode.window.showQuickPick(makeQuickPick(clipboardArray)).then((item) => {
-                console.log(item)
-                if (item.label === "-" && item.description === "Clear History") {
-                    clipboardArray = [];    // Clear clipboard history if selected
-                    return;
-                } else { pasteSelected(item) }
-        })}
-
+            window.showQuickPick(makeQuickPick(clipboardArray)).then((item) => { pasteSelected(item); });
+        }
     }));
 
-    disposableArray.push(vscode.commands.registerCommand('extension.removeFromClipboard', () => {
-        vscode.window.showQuickPick(makeQuickPick(clipboardArray)).then((item)=>{
-            console.log(item.description);
-            console.log(clipboardArray);
-            toBeRemoved = true;
-            vscode.window.showQuickPick(makeQuickPick(clipboardArray, item));
-            vscode.window.setStatusBarMessage("removed from clipboard!");
-            console.log(clipboardArray);
-        });
+    disposableArray.push(commands.registerCommand('extension.removeFromClipboard', () => {
+        if (clipboardArray.length == 0) {
+            window.setStatusBarMessage("No items in clipboard");
+            window.showQuickPick(makeQuickPick(clipboardArray));
+            return;
+        } else {
+            let currentQuickPick = makeQuickPick(clipboardArray, true);
+            window.showQuickPick(currentQuickPick).then((item)=>{
+                if (item.label === "0" && item.description === "Clear All History") {
+                    clipboardArray = [];    // Clear clipboard history if selected
+                    window.setStatusBarMessage("Clipboard history cleared");
+                    return;
+                } else {
+                    let removedQuickPick = makeQuickPick(removeQuickPickItem(clipboardArray, item), true);
+                    window.setStatusBarMessage("Removed from clipboard");
+                }
+            });
+        }
+    }));
+    
+    disposableArray.push(commands.registerCommand('extension.editClipboard', () => {
+        if (clipboardArray.length == 0) {
+            window.setStatusBarMessage("No items in clipboard");
+            return;
+        } else {
+            let currentQuickPick = makeQuickPick(clipboardArray);
+            window.showQuickPick(currentQuickPick).then((item) => {
+                let text = item.description;
+                window.showInputBox({ value: item.description.toString() })
+                    .then(val => {
+                        let editedQuickPick = makeQuickPick(editQuickPickItem(clipboardArray, item, val));
+                        window.setStatusBarMessage("Edited clipboard item");
+                    });
+            })
+        }
     }));
 
     context.subscriptions.concat(disposableArray);
